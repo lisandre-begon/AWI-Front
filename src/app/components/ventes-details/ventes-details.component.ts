0722e4039d2ef28586d2a473764aecf29c6cb777
mathilde.ducasse <mathilde.ducasse@etu.umontpellier.fr>
2024-12-25 17:47:35 +0100

    Un peu de css par là

--- src/app/components/ventes-details/ventes-details.component.ts
+++ src/app/components/ventes-details/ventes-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnChanges, OnInit} from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 //import { Vente } from '../../models/vente';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -6,13 +6,36 @@ import { FormGroup, FormControl, FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TransactionService } from '../../service/transaction.service'
 
+enum statuts{
+  depot,
+  vente,
+  pasEncoreVendu,
+}
+
 @Component({
   selector: 'app-ventes-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './ventes-details.component.html',
   styleUrl: './ventes-details.component.css'
 })
-export class VentesDetailsComponent {
+export class VentesDetailsComponent implements OnInit, OnChanges{
+
+  // statut: statuts;
+  // gestionnaire: number;
+  // proprietaire: number;
+  // acheteur: number;
+  // date_transaction: Date;
+  // remise: number;
+  // prix_total: number;
+  // frais: number;
+  // jeux: number[];
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
