411f8892052f3403cbf555934189f717bf3dfe29
mathildeawi <Mathildexducasse@gmail.com>
2025-01-24 16:47:34 +0100

    work on api.service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Vendeur } from '../models/Vendeur';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,8 @@ export class ApiService {
     return this.http.get<any>(`${this.apiUrl}/message`);
   }
 
-  getAllvendeur(): Observable<any> {
+  //Reccuperer les vendeurs : 
+  getAllVendeur(): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/api/vendeur/`);
   }
 
@@ -23,5 +25,62 @@ export class ApiService {
     return this.http.get<any>(`${this.apiUrl}/api/vendeur/:${id}`);
    }
 
+   //Create Vendeur : 
 
+  createVendeur(vendeur : Vendeur ):void{}
+
+   //Reccuperer les acheteurs : 
+  getAllAcheteurs(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/acheteur/`);
+  }
+
+  getAcheteurById(id : String): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/acheteur/:${id}`);
+   }
+
+   //Reccuperer les jeux : 
+  getAllJeu(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/jeu/`);
+  }
+
+  getJeuById(id : String): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/jeu/:${id}`);
+   }
+
+   //Reccuperer les gestionnaires :
+  getAllgest(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/gestionnaire/`);
+  }
+
+  getGestById(id : String): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/gestionnaire/:${id}`);
+   }
+
+   //Reccuperer les typeJeu : 
+  getAllTypeJeu(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/typejeu/`);
+  }
+
+  getTypeJeuById(id : String): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/typejeu/:${id}`);
+   }
+
+   //Reccuperer les catégorie : 
+  getAllCategorie(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/categorie/`);
+  }
+
+  getCategorieById(id : String): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/categorie/:${id}`);
+   }
+
+
+   //Reccuperer les transactions : 
+  getAllTransaction(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/api/transaction/`);
+  }
+
+  getTransactionById(id : String): Observable<any>{
+    return this.http.get<any>(`${this.apiUrl}/api/transaction/:${id}`);
+   }
 }
