fccd4e38c8bc3f80684581f25cfd17550b11ba61
lisandre-begon <lisandre.begon1@gmail.com>
2025-02-11 02:03:33 +0100

    Resolution erreur avec environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://awiback.herokuapp.com/api' // Replace with your actual Heroku backend URL
+    apiUrl: 'https://awiback-30abadc2c48e.herokuapp.com/api' // Replace with your actual Heroku backend URL
   };
   
