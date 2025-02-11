fbd0cba829945d52ca2969792295088bcbd3bb01
lisandre-begon <lisandre.begon1@gmail.com>
2025-02-11 11:28:20 +0100

    resolution conflit avec le auth.service et session.service

--- src/app/service/session.service.ts
+++ src/app/service/session.service.ts
@@ -9,26 +9,24 @@ import { environment } from '../../environments/environment';
 })
 export class SessionService {
 
+  private readonly apiUrl = environment.apiUrl;
 
-private readonly apiUrl = environment.apiUrl;
-
-
-constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient) { }
 
   getNextPlannedSession(): Observable<Session>{
-    return this.http.get<Session>(this.apiUrl + 'api/nextsession');
+    return this.http.get<Session>(this.apiUrl + '/nextsession');
   }
 
   isSessionActive(): Observable<Boolean>{
-    return this.http.get<{isActive : boolean}>(this.apiUrl + 'api/activeSession').pipe(map((response) => response.isActive));
+    return this.http.get<{isActive : boolean}>(this.apiUrl + '/activeSession').pipe(map((response) => response.isActive));
   }
 
   getSessionPlanifie(): Observable<Session>{
-    return this.http.get<Session>(this.apiUrl + 'api/planified');
+    return this.http.get<Session>(this.apiUrl + '/planified');
   }
 
   getSessionEnCours(): Observable<Session>{
-    return this.http.get<Session>(this.apiUrl + 'api/encours');
+    return this.http.get<Session>(this.apiUrl + '/encours');
   }
 
   addSession(session : Session): Observable<Session>{
@@ -36,10 +34,10 @@ constructor(private http : HttpClient) { }
   }
 
   deleteSession(idSession : string): Observable<Session>{
-    return this.http.delete<Session>(this.apiUrl + 'api/' + idSession)
+    return this.http.delete<Session>(this.apiUrl + '/' + idSession)
   }
 
   getAllSession(): Observable<Session[]>{
-    return this.http.get<Session[]>(this.apiUrl + 'api/');
+    return this.http.get<Session[]>(this.apiUrl + '/');
   }
 }
