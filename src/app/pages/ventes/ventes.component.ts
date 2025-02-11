8a05b7fb66a3dbeb9537aa320162e502344ea67d
lisandre-begon <lisandre.begon1@gmail.com>
2025-02-12 00:31:03 +0100

    erreur dans page vente

--- src/app/pages/ventes/ventes.component.ts
+++ src/app/pages/ventes/ventes.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { VenteDetailsComponent } from "../../components/ventes-details/ventes-details.component";
+import { VentesDetailsComponent } from "../../components/ventes-details/ventes-details.component";
 
 @Component({
   selector: 'app-ventes',
   standalone: true,
-  imports: [VenteDetailsComponent],
+  imports: [VentesDetailsComponent],
   templateUrl: './ventes.component.html',
   styleUrl: './ventes.component.css'
 })
